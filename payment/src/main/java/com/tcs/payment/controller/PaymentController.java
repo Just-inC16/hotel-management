18eff9b80930331033a1828c315b268f12d91cf8
Justin <justin.chan.3719@gmail.com>
2026-09-16 21:53:41 -0400

    Fixed compilation errors

--- payment/src/main/java/com/tcs/payment/controller/PaymentController.java
+++ payment/src/main/java/com/tcs/payment/controller/PaymentController.java
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream:payment/src/main/java/com/tcs/payment/controller/PaymentController.java
 package com.tcs.payment.controller;
-=======
-package com.tcs.payment;
->>>>>>> Stashed changes:payment/src/main/java/com/tcs/payment/PaymentController.java
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +8,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< Updated upstream:payment/src/main/java/com/tcs/payment/controller/PaymentController.java
 import com.tcs.payment.model.Payment;
-=======
->>>>>>> Stashed changes:payment/src/main/java/com/tcs/payment/PaymentController.java
 import com.tcs.payment.service.PaymentService;
 
 @RestController
 @RequestMapping("/api/v1/payments")
 public class PaymentController {
 
-<<<<<<< Updated upstream:payment/src/main/java/com/tcs/payment/controller/PaymentController.java
 	private final PaymentService paymentService;
-=======
-	private PaymentService paymentService;
->>>>>>> Stashed changes:payment/src/main/java/com/tcs/payment/PaymentController.java
 
 	public PaymentController(PaymentService paymentService) {
 		this.paymentService = paymentService;
@@ -34,11 +23,7 @@ public class PaymentController {
 
 	@PostMapping
 	public ResponseEntity<Payment> makePayment(@RequestBody Payment payment) {
-<<<<<<< Updated upstream:payment/src/main/java/com/tcs/payment/controller/PaymentController.java
 		return paymentService.makePayment(payment);
-=======
-		return ResponseEntity.ok(paymentService.makePayment(payment));
->>>>>>> Stashed changes:payment/src/main/java/com/tcs/payment/PaymentController.java
 	}
 
 	@GetMapping("/{id}")
--- reservation/src/main/java/com/tcs/reservation/service/ReservationService.java
+++ reservation/src/main/java/com/tcs/reservation/service/ReservationService.java
@@ -1,32 +1,25 @@
 package com.tcs.reservation.service;
 
-<<<<<<< Updated upstream:reservation/src/main/java/com/tcs/reservation/service/ReservationService.java
 import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-=======
->>>>>>> Stashed changes:reservation/src/main/java/com/tcs/reservation/ReservationController.java
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.tcs.reservation.Dto.HotelManagement;
 import com.tcs.reservation.Dto.Notification;
 import com.tcs.reservation.Dto.Payment;
-<<<<<<< Updated upstream:reservation/src/main/java/com/tcs/reservation/service/ReservationService.java
 import com.tcs.reservation.feign.HotelManagementClient;
 import com.tcs.reservation.feign.NotificationClient;
 import com.tcs.reservation.feign.PaymentClient;
 import com.tcs.reservation.model.Reservation;
 import com.tcs.reservation.repository.ReservationRepository;
-=======
-import com.tcs.reservation.service.ReservationService;
->>>>>>> Stashed changes:reservation/src/main/java/com/tcs/reservation/ReservationController.java
 
 @Service
 public class ReservationService {
 
-<<<<<<< Updated upstream:reservation/src/main/java/com/tcs/reservation/service/ReservationService.java
 	private final ReservationRepository reservationRepository;
 	private final HotelManagementClient hotelManagementClient;
 	private final PaymentClient paymentClient;
@@ -86,47 +79,12 @@ public class ReservationService {
 
 	public ResponseEntity<Notification> sendNotification(Notification notification) {
 		return notificationClient.sendNotification(notification);
-=======
-	private ReservationService reservationService;
-
-	public ReservationController(ReservationService reservationService) {
-		this.reservationService = reservationService;
-	}
-
-	@PostMapping
-	public ResponseEntity<?> makeReservation(@RequestBody Reservation reservation) {
-		return ResponseEntity.ok(reservationService.makeReservation(reservation));
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getReservationById(@PathVariable Long id) {
-		return ResponseEntity.ok(reservationService.getReservationById(id));
-	}
-
-	@PostMapping("/reserveHotel")
-	public ResponseEntity<Notification> reserveHotel(@RequestBody Reservation reservation) {
-		return reservationService.reserveHotel(reservation);
-	}
-
-	@PostMapping("/sendPayment")
-	public ResponseEntity<Payment> sendPayment(@RequestBody Payment payment) {
-		return reservationService.sendPayment(payment);
-	}
-
-	@PostMapping("/sendNotification")
-	public ResponseEntity<Notification> sendNotification(@RequestBody Notification notification) {
-		return reservationService.sendNotification(notification);
->>>>>>> Stashed changes:reservation/src/main/java/com/tcs/reservation/ReservationController.java
 	}
 
 	@KafkaListener(topics = "reserve-room")
 	public String getNotifications(Reservation reservation) {
-<<<<<<< Updated upstream:reservation/src/main/java/com/tcs/reservation/service/ReservationService.java
 		System.out.println("Recieved account event" + reservation.toString());
 
 		return "Successful" + reservation.toString();
-=======
-		return reservationService.consumeReservation(reservation);
->>>>>>> Stashed changes:reservation/src/main/java/com/tcs/reservation/ReservationController.java
 	}
 }
